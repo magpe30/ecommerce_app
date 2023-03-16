73d2c2d52cc7cf0bb06637cc16614a495a1e1804
magpe30 <mpelka892@gmail.com>
2023-03-16 20:54:14 +0000

    fix: fix the secrets

--- candles_django/candles_django/settings.py
+++ candles_django/candles_django/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+from decouple import config
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@s8lry^9x4w6vu-p&k2i(04@+57qz*kmbxx*3g^*5(d1rb$#(q'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
